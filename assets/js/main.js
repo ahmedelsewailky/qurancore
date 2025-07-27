abf077ada9cd7e18155bc0848eb0c20f1989bc11
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2025-07-28 00:09:31 +0300

    Courses and testimonails sectio completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,3 +120,26 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 3,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false,
+            loop: false
+        }
+    }
+});
